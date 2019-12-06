f01c71cf394c3676ebf62e439cd609eb572def3c
Joshu Groves <joshgroves@outlook.com>
2019-12-06 15:03:30 +1300

    Getting hit
    Code u need.
    private void OnCollisionEnter(Collision other)
        {
            if (other.collider.tag == "Player")
            {
                Evil = GameObject.Find("Evil");
                Destroy(Evil);
                Destroy(gameObject);
    
            }      
    
        }

---
+++ HitResult.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitResult : MonoBehaviour
+{
+    public GameObject Evil;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Evil.transform.childCount == 0)
+        {
+            DestroyImmediate(Evil);
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.tag == "Player")
+        {
+            Evil = GameObject.Find("Evil");
+            Destroy(Evil);
+            Destroy(gameObject);
+
+        }      
+
+    }
+    
+
+}
